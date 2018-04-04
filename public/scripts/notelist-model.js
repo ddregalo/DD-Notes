c537acb3190f8d8d5f85901909d13dcdb5857536
danielcampos <ddregalo@gmail.com>
2018-04-04 10:05:50 +0100

    TDD newNote function in NoteList - creates and stores new Note model - test passed

--- public/scripts/notelist-model.js
+++ public/scripts/notelist-model.js
@@ -3,7 +3,16 @@
     this.notesDb = []
   };
   NoteList.prototype.allNotes = function () {
+    // for (let i=0; i<this.notesDb.length; i+=1) {
+    //   document.getElementById("all_notes").innerHTML +=(this.notesDb[i].content);
+    // };
     return this.notesDb;
+  };
+  NoteList.prototype.newNote = function (text) {
+    let note = new Note(text);
+    console.log(note);
+    return this.notesDb.push(note);
   };
   exports.NoteList = NoteList;
 })(this);
+console.log(`This is: ${this}`);
--- tester/tests/note-tests.js
+++ tester/tests/note-tests.js
@@ -14,3 +14,15 @@ function testNoteListIsArray() {
   test.isEqual(noteList.allNotes() === noteList.notesDb);
 };
 testNoteListIsArray();
+
+// Tests that newNote creates and stores a new single note model
+function testNewNote() {
+  document.getElementById("result").innerHTML += "Test // NoteList newNote function creates and stores new note model:";
+  let noteList = new NoteList();
+  let text = "Checkmate 1-2 1-2!!";
+  console.log(`NotesDB before newNOte function: ${noteList.allNotes()}`)
+  noteList.newNote(text);
+  console.log(`NotesDB first array value: ${noteList.notesDb[0]}`)
+  test.isEqual(noteList.notesDb[0].content === text);
+};
+testNewNote();
